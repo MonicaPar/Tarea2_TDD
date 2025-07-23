91ce23413c6cc5d9afac6e0ca59ea9e60998a5b4
MonicaPar <20002603@galileo.edu>
2025-07-22 20:45:22 -0600

    La cadena recibida, es la que se devuelve

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,4 +1,8 @@
 def string_calculator(cadena):
     if cadena == "":
         return 0
-    
+    if len(cadena) == 1:
+        if cadena.isdigit():
+            return int(cadena)
+        else:
+            return cadena
