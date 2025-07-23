c1292358d061824f19017524d61bfcbdee5d08f5
MonicaPar <20002603@galileo.edu>
2025-07-22 20:43:45 -0600

    Cadena única devuelve el mismo valor de la cadena

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -2,3 +2,6 @@ from src.calculadora import string_calculator
 
 def test_empty_string_returns_zero():
     assert string_calculator("") == 0
+
+def cadena_unica():
+    assert string_calculator("m") == "m"
