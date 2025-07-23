23091e9a6695d9e3223e5a34149612789531ce6f
MonicaPar <20002603@galileo.edu>
2025-07-22 20:48:49 -0600

    Hace suma de la cadena completa

--- src/calculadora.py
+++ src/calculadora.py
@@ -2,7 +2,7 @@ def string_calculator(cadena):
     if cadena == "":
         return 0
     if len(cadena) == 1:
-        if cadena.isdigit():
-            return int(cadena)
-        else:
-            return cadena
+        if "," in cadena:
+            return sum(int(x) for x in cadena.split(",") if x.isdigit())
+        return int(cadena)
+
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -4,4 +4,7 @@ def test_empty_string_returns_zero():
     assert string_calculator("") == 0
 
 def cadena_unica():
-    assert string_calculator("m") == "m"
+    assert string_calculator("2") == "2"
+
+def varios_valores():
+    assert string_calculator("2,1") == 3
